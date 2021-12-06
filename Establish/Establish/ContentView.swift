5ea7011c81432ab2f45efb2bfb8a7501a284db5e
Joshua Brown <joshua.rd.brown2001@gmail.com>
2021-12-06 14:53:23 +0000

    #1 - Project setup

---
+++ Establish/Establish/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Establish
+//
+//  Created by Joshua Brown on 27/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
